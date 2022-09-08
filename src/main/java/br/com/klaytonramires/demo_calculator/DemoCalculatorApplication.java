924d7b8813c4fc759430d199fd6112d6cbac2d3a
kramires <ramires03@hotmail.com>
2022-09-08 11:28:22 -0300

    first commit

---
+++ src/main/java/br/com/klaytonramires/demo_calculator/DemoCalculatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.klaytonramires.demo_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCalculatorApplication.class, args);
+	}
+
+}
